e270b530fd50938331c8e3c2856cd0abc9ca1953
Calvin Yang <calvinyang98@hotmail.com>
2016-09-29 22:13:43 +0800

    Invert dependency from Logic to Storage

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,34 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public abstract class Storage {
+	
+	/**
+     * Signals that the given file path does not fulfill the storage filepath constraints.
+     */
+    public static class InvalidStorageFilePathException extends IllegalValueException {
+        public InvalidStorageFilePathException(String message) {
+            super(message);
+        }
+    }
+    
+    /**
+     * Saves all data to this storage file.
+     *
+     * @throws StorageOperationException if there were errors converting and/or storing data to file.
+     */
+    public abstract void save(AddressBook addressBook) throws StorageOperationException;
+    
+    /**
+     * Loads data from this storage file.
+     *
+     * @throws StorageOperationException if there were errors reading and/or converting data from file.
+     */
+    public abstract AddressBook load() throws StorageOperationException;
+    
+    public abstract String getPath();
+
+}
